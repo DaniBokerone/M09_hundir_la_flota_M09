3e14110464133caacf44df8d90ccaf30b7a5dfb4
DaniBokerone <danibokerone@gmail.com>
2024-10-09 17:40:19 +0200

    Update project
    -Added proxmox configurations
    -Updated controllers to be similar to the example

--- src/main/java/com/project/ClientFX.java
+++ src/main/java/com/project/ClientFX.java
@@ -1,20 +1,28 @@
 package com.project;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.animation.PauseTransition;
+import javafx.scene.paint.Color;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
 
 public class ClientFX extends Application {
 
     public static UtilsWS wsClient;
 
-    public static int port = 3000;
-    public static String protocol = "http";
-    public static String host = "localhost";
-    public static String protocolWS = "ws";
+    public static String clientId = "";
+    public static CtrlConfig ctrlConfig;
+    public static CtrlWait ctrlWait;
+    public static CtrlPlay ctrlPlay;
 
     public static void main(String[] args) {
 
@@ -29,7 +37,13 @@ public class ClientFX extends Application {
         final int windowHeight = 300;
 
         UtilsViews.parentContainer.setStyle("-fx-font: 14 arial;");
-        UtilsViews.addView(getClass(), "ViewSockets", "/assets/viewSockets.fxml");
+        UtilsViews.addView(getClass(), "ViewConfig", "/assets/viewConfig.fxml");
+        UtilsViews.addView(getClass(), "ViewWait", "/assets/viewWait.fxml");
+        UtilsViews.addView(getClass(), "ViewPlay", "/assets/viewPlay.fxml");
+
+        ctrlConfig = (CtrlConfig) UtilsViews.getController("ViewConfig");
+        ctrlWait = (CtrlWait) UtilsViews.getController("ViewWait");
+        ctrlPlay = (CtrlPlay) UtilsViews.getController("ViewPlay");
 
         Scene scene = new Scene(UtilsViews.parentContainer);
         
@@ -45,24 +59,90 @@ public class ClientFX extends Application {
             Image icon = new Image("file:/icons/icon.png");
             stage.getIcons().add(icon);
         }
-
-        // Iniciar WebSockets
-        wsClient = UtilsWS.getSharedInstance(protocolWS + "://" + host + ":" + port);
-        wsClient.onMessage((response) -> {
-            
-            // JavaFX necessita que els canvis es facin des de el thread principal
-            Platform.runLater(()->{ 
-                // Fer aquí els canvis a la interficie
-                JSONObject msgObj = new JSONObject(response);
-                CtrlSockets ctrl = (CtrlSockets) UtilsViews.getController("ViewSockets");
-                ctrl.receiveMessage(msgObj);
-            });
-        });
     }
 
     @Override
     public void stop() { 
-        wsClient.forceExit();
+        if (wsClient != null) {
+            wsClient.forceExit();
+        }
         System.exit(1); // Kill all executor services
+    }
+
+    public static void pauseDuring(long milliseconds, Runnable action) {
+        PauseTransition pause = new PauseTransition(Duration.millis(milliseconds));
+        pause.setOnFinished(event -> Platform.runLater(action));
+        pause.play();
+    }
+
+    public static <T> List<T> jsonArrayToList(JSONArray array, Class<T> clazz) {
+        List<T> list = new ArrayList<>();
+        for (int i = 0; i < array.length(); i++) {
+            T value = clazz.cast(array.get(i));
+            list.add(value);
+        }
+        return list;
+    }
+
+    public static void connectToServer() {
+
+        ctrlConfig.txtMessage.setTextFill(Color.BLACK);
+        ctrlConfig.txtMessage.setText("Connecting ...");
+    
+        pauseDuring(1500, () -> { // Give time to show connecting message ...
+
+            String protocol = ctrlConfig.txtProtocol.getText();
+            String host = ctrlConfig.txtHost.getText();
+            String port = ctrlConfig.txtPort.getText();
+            wsClient = UtilsWS.getSharedInstance(protocol + "://" + host + ":" + port);
+    
+            wsClient.onMessage((response) -> { Platform.runLater(() -> { wsMessage(response); }); });
+            wsClient.onError((response) -> { Platform.runLater(() -> { wsError(response); }); });
+        });
+    }
+   
+    private static void wsMessage(String response) {
+        // System.out.println(response);
+        JSONObject msgObj = new JSONObject(response);
+        switch (msgObj.getString("type")) {
+            case "clients":
+                if (clientId == "") {
+                    clientId = msgObj.getString("id");
+                }
+                if (UtilsViews.getActiveView() != "ViewWait") {
+                    UtilsViews.setViewAnimating("ViewWait");
+                }
+                List<String> stringList = jsonArrayToList(msgObj.getJSONArray("list"), String.class);
+                if (stringList.size() > 0) { ctrlWait.txtPlayer0.setText(stringList.get(0)); }
+                if (stringList.size() > 1) { ctrlWait.txtPlayer1.setText(stringList.get(1)); }
+                break;
+            case "countdown":
+                int value = msgObj.getInt("value");
+                String txt = String.valueOf(value);
+                if (value == 0) {
+                    UtilsViews.setViewAnimating("ViewPlay");
+                    txt = "GO";
+                }
+                ctrlWait.txtTitle.setText(txt);
+                break;
+            case "serverMouseMoving":
+                ctrlPlay.setPlayersMousePositions(msgObj.getJSONObject("positions"));
+                break;
+            case "serverSelectableObjects":
+                ctrlPlay.setSelectableObjects(msgObj.getJSONObject("selectableObjects"));
+                break;
+        }
+    }
+
+    private static void wsError(String response) {
+
+        String connectionRefused = "Connection refused";
+        if (response.indexOf(connectionRefused) != -1) {
+            ctrlConfig.txtMessage.setTextFill(Color.RED);
+            ctrlConfig.txtMessage.setText(connectionRefused);
+            pauseDuring(1500, () -> {
+                ctrlConfig.txtMessage.setText("");
+            });
+        }
     }
 }
